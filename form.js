40e05032dec01a337adabbf1bea05a15a2ba925e
JatsukinG <113560403+JatsukinG@users.noreply.github.com>
2022-09-21 21:41:03 +0000

    Update form.js

--- form.js
+++ form.js
@@ -19,12 +19,25 @@ formSubmit.addEventListener("submit", (e) => {
     lista.unshift(tarea);
 
     lista.map((obj) => {
-      let tareaBox = document.createElement("label");
-      tareaBox.innerHTML = obj.text;
+      let tareaBox = document.createElement("div");
       tareaBox.classList.add("tareaBox");
       tareasBox.append(tareaBox);
+      let p = document.createElement("p");
+      p.innerHTML = obj.text;
+      tareaBox.append(p);
+      let btn = document.createElement("button");
+      btn.classList.add("btnState");
+      btn.innerHTML = "Uncompleted";
+      tareaBox.append(btn);
     });
   }
 
   textInput.value = "";
 });
+
+/*const btnState = document.querySelector("#btnState");
+
+const completed = () => {
+  btnState.innerHTML = "Completed";
+  btnState.classList.toggle("btn-state-completed");
+};*/
